78f3adce358c6e4f07bcb77eabeecc7525c95f80
Eric NIYONGABO <niyo@Erics-MacBook-Pro-4.local>
2019-10-18 20:07:21 +0200

    Split ui into 2

---
+++ src/Left.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Left">
+      <h1>Hello Left</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Right.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import img from './images/pexels-photo-1591447.jpeg';
+
+function App() {
+  return (
+    <div className="Right">
+      <img src={img} alt="img" style={style}/>
+    </div>
+  );
+}
+
+const style = {
+    width: '100%',
+    height: '100%'
+}
+
+export default App;
