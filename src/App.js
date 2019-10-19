fbb551f1f1eb0e66a1ecfeadcaafd29fe7902f1a
hezronkimutai <hezron.kimutai@andela.com>
2019-10-19 18:45:02 +0200

    Fix the left

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 import Left from './Left';
 import Right from './Right';
 import './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <Right />
-      <Left />
-    </div>
-  );
+
+class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = {
+          data : []
+      }
+  }
+
+
+  componentDidMount(){
+
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=dodoma&APPID=7f6a0fe6f25df85a9176f605964e2fb0`)
+    .then( (response) => {
+      console.log(response.data)
+      this.setState({ 
+        data: response.data
+      })
+    })
+    .catch( (error) => {
+      console.log(error);
+    })
+    .finally(() => {
+    });
+      }
+      
+  render(){
+      console.log(this.state)
+      return(
+        <div className="App">
+        <Right />
+        <Left />
+      </div>
+      )
+  }
 }
 
-export default App;
+export default App
--- src/Right.js
+++ src/Right.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import './App.css';
-import img from './images/pexels-photo-1591447.jpeg';
+import img from './images/pexels-photo-209831.jpeg';
+import six from './images/6.svg'
 
 function App() {
   return (
-    <div className="Right">
-      <img src={img} alt="img" style={style}/>
-    </div>
+      <div className="Right"style={style}>
+      
+      <div className="left-bottom">
+
+
+        <div className="temp">000</div>
+
+        <div className="city-time">
+          <div className="city">Eldoret</div>
+          <div className="time">6:56</div>
+        </div>
+
+        <div className="icon-desc">
+          <div className="icon"><img src={six} alt="icon" style={icon}/></div>
+          <div className="desc">rainy</div>
+        </div>
+
+
+
+        </div>
+        </div>
   );
 }
-
+const icon = {
+  width: '50px',
+  height: '50px'
+}
 const style = {
+  backgroundImage:`url(${img})`,
+  backgroundSize: '100% 100%',
     width: '100%',
     height: '100%'
 }
